93288dfe74bc3ee1c058eefd324c11577d9f7201
eliseubaldo <eliseu_baldo@hotmail.com>
2015-08-14 11:32:14 -0400

    first commit

---
+++ script/script.js
@@ -0,0 +1,43 @@
+// create the module and name it scotchApp
+        // also include ngRoute for all our routing needs
+var myApp = angular.module('myApp',['ngRoute']);
+
+
+// Configure Routing
+
+myApp.config(function($routeProvider){
+
+	$routeProvider
+	.when('/', {
+		templateUrl:'pages/home.html',
+		controller:'mainController'
+	})
+	.when('/about', {
+		templateUrl:'pages/about.html',
+		controller:'aboutController'
+
+	})
+	.when('/contact',{
+		templateUrl:'pages/contact.html',
+		controller:'contactController'
+	});
+
+});
+
+
+// Controllers for each route
+
+
+	myApp.controller('mainController', function($scope){
+
+		$scope.message = 'Do not go where the path may lead, go instead where there is no path and leave a trail.';
+	});
+
+	myApp.controller('aboutController', function($scope){
+
+		$scope.message = "We are made wise not by the recollection of our past, but by the responsibility for our future.";
+	});
+
+	myApp.controller('contactController', function($scope){
+		$scope.message = 'If youre trying to achieve, there will be roadblocks. Ive had them; everybody has had them. But obstacles dont have to stop you. If you run into a wall, dont turn around and give up. Figure out how to climb it, go through it, or work around it.';
+	});
