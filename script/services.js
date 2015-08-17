0a764635342c1723d6dc1f062fc5c7486843e6c8
eliseubaldo <eliseu_baldo@hotmail.com>
2015-08-17 13:43:33 -0400

    update services

--- script/services.js
+++ script/services.js
@@ -4,6 +4,8 @@ myApp.factory('getSomeData', function($http){
 
 	factory.getContact = function(id){
 		return $http.get('http://www.mocky.io/v2/55d218ee5733d7c41121e95d')
+		// http://www.mocky.io/ Mock your HTTP responses to test your REST API
+
 	}
 
 	return factory;
